8fe33ca48e53f332ef7f250dd6b77394de06812e
wigilin <wigilin@gmail.com>
2021-08-12 18:08:01 +0800

    no message

--- js/befRef.js
+++ js/befRef.js
@@ -4,20 +4,24 @@ $('#newsRef_imp .mainMsg-title').text('Tin quan trọng');
 var newsRef_Data = [{
     img: 'images/newsRef/newsRef15-01.jpg',
     txt: 'Xác nhận phí chuyển nhượng của Saul, Man Utd đối đầu 2 ông lớn EPL',
-    href: 15,
+    href: 1,
+}, {
+    img: 'images/newsRef/newsRef15-01.jpg',
+    txt: 'Xác nhận phí chuyển nhượng của Saul, Man Utd đối đầu 2 ông lớn EPL',
+    href: 2,
 }, {
 
     img: 'images/newsRef/newsRef16-01.jpg',
     txt: 'Rob Holding muốn Arsenal đi theo con đường của Liverpool',
-    href: 16,
+    href: 3,
 }, {
     img: 'images/newsRef/newsRef17-01.jpg',
     txt: 'Bán Sancho chiêu mộ thêm 3 tân binh, đội hình Dortmund mùa tới ra sao?',
-    href: 17,
+    href: 4,
 }, {
     img: 'images/newsRef/newsRef18-01.jpg',
     txt: 'Hạ màn vòng bảng Olympic: Sao Arsenal nhạt nhòa; Real nở mày nở mặt',
-    href: 18,
+    href: 5,
 }, ]
 
 newsRef_Data.forEach(function(data) {
@@ -29,5 +33,3 @@ newsRef_Data.forEach(function(data) {
                      </li>`
     $('#newsRef_hot ul,#newsRef_imp ul').append(combination);
 })
-
-
